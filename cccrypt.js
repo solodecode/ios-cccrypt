e64a01b56851bc37b8f2b29d557f5106dc27d2f4
solodecode <47976752+solodecode@users.noreply.github.com>
2023-05-10 18:24:55 +0300

    Create cccrypt.js

---
+++ cccrypt.js
@@ -0,0 +1,20 @@
+var encrypt_func = Module.findExportByName("libcommonCrypto.dylib", "CCCrypt");
+
+Interceptor.attach(encrypt_func, {
+  onEnter: function (args) {
+    console.log("[+] CCCrypt function called");
+    console.log("[+] CCOperation: " + args[0].toInt32());
+    console.log("[+] CCAlgorithm: " + args[1].toInt32());
+    console.log("[+] CCOptions: " + args[2].toInt32());
+    console.log("[+] key: \n" + hexdump(ptr(args[3]), { length: args[4].toInt32() }));
+    console.log("[+] keyLength: \n" + args[4].toInt32());
+    console.log("[+] iv: \n" + hexdump(ptr(args[5]), { length: 16 }));
+    console.log("[+] dataIn: \n" + hexdump(ptr(args[6]), { length: args[7].toInt32() }));
+    console.log("[+] dataInLength: " + args[7].toInt32());
+	this.dataOutPtr = args[8];
+    this.dataOutLengthPtr = args[10];
+  },
+  onLeave: function (retval) {
+    console.log("[+] dataOut: \n" + hexdump(ptr(this.dataOutPtr), { length: ptr(this.dataOutLengthPtr).readUInt() }));
+  }
+});
